6d7d84fabe09062613d36785c0c4e4a708798f58
Brandon Blair <brandon.blair@rewardstyle.com>
2018-09-08 13:09:08 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elegantcasing',
+    version='1.0.1',
+    description='Change your text to title-case',
+    author='Brandon Blair',
+    author_email='cbrandon.blair@gmail.com',
+    url='https://github.com/BrandonBlair/elegantframeworks',
+    packages=find_packages()
+)
---
+++ titlecase/__init__.py
@@ -0,0 +1,1 @@
+from .titlecase import title_case
