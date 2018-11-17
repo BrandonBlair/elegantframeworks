38fd3918084c2f03a9a9058a601fd79201433609
Brandon Blair <cbrandon.blair@gmail.com>
2018-11-17 14:08:59 -0600

    Initial commit

---
+++ tests/test_chemistry_results.py
@@ -0,0 +1,34 @@
+import time
+
+
+def test_result_1_completes_as_expected():
+    time.sleep(5)
+    print("Result 1 has completed")
+
+def test_result_2_completes_as_expected():
+    time.sleep(5)
+    print("Result 2 has completed")
+
+def test_result_3_completes_as_expected():
+    time.sleep(5)
+    print("Result 3 has completed")
+
+def test_result_4_completes_as_expected():
+    time.sleep(5)
+    print("Result 4 has completed")
+
+def test_result_5_completes_as_expected():
+    time.sleep(5)
+    print("Result 5 has completed")
+
+def test_result_6_completes_as_expected():
+    time.sleep(5)
+    print("Result 6 has completed")
+
+def test_result_7_completes_as_expected():
+    time.sleep(5)
+    print("Result 7 has completed")
+
+def test_result_8_completes_as_expected():
+    time.sleep(5)
+    print("Result 8 has completed")
